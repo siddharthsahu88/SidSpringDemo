b0004c19807cb67684f508212c14ce2b525251ec
siddharth_sahu <siddharthsahu1988@yahoo.com>
2018-11-01 20:03:00 +0530

    Oracle Config

--- src/main/java/com/sid/spring/config/OracleConfig.java
+++ src/main/java/com/sid/spring/config/OracleConfig.java
@@ -1,30 +1,30 @@
-package com.sid.SpringBootRestApp;
+package com.sid.spring.config;
 
 import java.sql.SQLException;
 
 import javax.sql.DataSource;
-import javax.validation.constraints.NotNull;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import oracle.jdbc.pool.OracleDataSource;
 
 @Configuration
-@ConfigurationProperties("oracle")
+@PropertySource("classpath:application.properties")
 public class OracleConfig {
 
-	@NotNull
+	@Value("${spring.datasource.username}")
 	private String username;
 
-	@NotNull
+	@Value("${spring.datasource.password}")
 	private String password;
 
-	@NotNull
+	@Value("${spring.datasource.url}")
 	private String url;
 
-	public void setUsername(String username) {
+	/*public void setUsername(String username) {
 		this.username = username;
 	}
 
@@ -34,9 +34,9 @@ public class OracleConfig {
 
 	public void setUrl(String url) {
 		this.url = url;
-	}
+	}*/
 
-	@Bean
+	@Autowired
 	public DataSource datasource() throws SQLException {
 
 		OracleDataSource ods = new OracleDataSource();
@@ -44,8 +44,8 @@ public class OracleConfig {
 		ods.setUser(username);
 		ods.setPassword(password);
 		ods.setURL(url);
-		ods.setImplicitCachingEnabled(true);
-		ods.setFastConnectionFailoverEnabled(true);
+		//ods.setImplicitCachingEnabled(true);
+		//ods.setFastConnectionFailoverEnabled(true);
 
 		return ods;
 	}
